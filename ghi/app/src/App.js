b231cee230de5724924cf08362f8a090ac8cef0f
John Agni <johncliffordagni@gmail.com>
2023-03-06 19:00:42 -1000

    Created lists for manufacturer, vehicle, and inventory

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import VehicleModelList from './inventory/VehicleModelList';
+import Manufacturerlist from './inventory/ManufacturerList';
+import AutomobileList from './inventory/AutomobileList';
 
-function App() {
+
+
+
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturers" element={<Manufacturerlist manufacturers={props.models} />} />
+          <Route path="models" element={<VehicleModelList models={props.models} />} />
+          <Route path="automobiles" element={<AutomobileList automobiles={props.automobiles} /> } />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/inventory/VehicleModelList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class VehicleModelList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            models: [] //intialize state with empty array
+        }
+    }
+
+    async componentDidMount() {
+        const url = "http://localhost:8100/api/models/"
+        const response = await fetch(url)
+        if (response.ok) {
+            let data = await response.json();
+            this.setState({models: data.models})
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Vehicle Models</h1>
+
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Manufacturer</th>
+                            <th>Name</th>
+                            <th>Picture</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.models.map(model => {
+                            return (
+                                <tr key={model.id}>
+                                    <td>{model.manufacturer.name}</td>
+                                    <td>{model.name}</td>
+                                    <td><img style={{height:"100px", width:"200px"}} src={model.picture_url}/></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default VehicleModelList;
