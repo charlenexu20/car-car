129b66eb4d1d4ea74217800d70ce52be676a1cbd
charlene <charlene.xu92@gmail.com>
2023-03-07 09:37:22 -0800

    complete inventory forms

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import ManufacturerForm from './inventory/ManufacturerForm';
+import VehicleModelForm from './inventory/VehicleModelForm';
+import AutomobileForm from './inventory/AutomobileForm';
 
 function App() {
   return (
@@ -9,6 +12,15 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturers">
+            <Route path="new" element={<ManufacturerForm />} />
+          </Route>
+          <Route path="models">
+            <Route path="new" element={<VehicleModelForm />} />
+          </Route>
+          <Route path="automobiles">
+            <Route path="new" element={<AutomobileForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/inventory/ManufacturerForm.js
+++ ghi/app/src/inventory/ManufacturerForm.js
@@ -1,9 +1,56 @@
 import React, { useEffect, useState} from 'react';
-
 function ManufacturerForm() {
+    const [name, setName] = useState('');
+
+    const handleNameChange = (event) => {
+        const value = event.target.value
+        setName(value)
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const data = {};
+        data.name = name;
+        console.log(data);
+
+        const manufacturerUrl = "http://localhost:8100/api/manufacturers/";
+        const fetchConfig = {
+            method: "post",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        const manufacturerResponse = await fetch(manufacturerUrl, fetchConfig);
+        if (manufacturerResponse.ok) {
+            const manufacturerData = await manufacturerResponse.json();
+            console.log(manufacturerData);
+
+            setName('');
+        }
+    }
+
+    useEffect(() => {
+    }, []);
+
     return (
-        <p>ManufacturerForm</p>
+        <div className="row">
+            <div className="offset-3 col-6">
+                <div className="shadow p-4 mt-4">
+                    <h1>Create a manufacturer</h1>
+                    <form onSubmit={handleSubmit} id="create-manufacturer-form">
+                        <div className="form-floating mb-3">
+                            <input value={name} onChange={handleNameChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
+                            <label htmlFor="name">Name</label>
+                        </div>
+                        <button className="btn btn-primary">Create</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default ManufacturerForm
+export default ManufacturerForm;
